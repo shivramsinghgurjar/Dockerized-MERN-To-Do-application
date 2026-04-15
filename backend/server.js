92314772733f34dd2a994953d0c4d50957c599c0
shivramsinghgurjar <148748849+shivramsinghgurjar@users.noreply.github.com>
2026-04-15 09:08:19 +0530

    Initial MERN DevOps Project

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://mongo:27017/todos");
+
+const Todo = mongoose.model("Todo", { text: String });
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post("/todos", async (req, res) => {
+  const todo = new Todo({ text: req.body.text });
+  await todo.save();
+  res.json(todo);
+});
+
+app.listen(5000, () => console.log("Backend running on port 5000"));
